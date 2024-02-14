634612aca535ddc4bbcbeb7d49720dc400a022ff
Nguyen Ha <nguyeh3@uci.edu>
2024-02-13 18:20:54 -0800

    create food and sleep fragment

---
+++ app/src/main/java/com/example/snaphealth/FoodRecommendation.kt
@@ -0,0 +1,15 @@
+package com.example.snaphealth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.ComponentActivity
+
+class FoodRecommendation :  ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_food_recommendation)
+    }
+}
--- app/src/main/java/com/example/snaphealth/MainActivity.kt
+++ app/src/main/java/com/example/snaphealth/MainActivity.kt
@@ -1,22 +1,10 @@
 package com.example.snaphealth
 
 import android.annotation.SuppressLint
-import android.app.Instrumentation
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import android.content.SharedPreferences
 import android.widget.Button
-import android.widget.RatingBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
-import java.time.LocalDate
-import java.time.LocalDate.now
 
 
 class MainActivity :  ComponentActivity() {
@@ -25,9 +13,21 @@ class MainActivity :  ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val switchButton = findViewById<Button>(R.id.button)
+        val switchButton = findViewById<Button>(R.id.hydration_switch)
         switchButton.setOnClickListener {
             val intent = Intent(this, Hydration::class.java)
+            startActivity(intent)
+        }
+
+        val switchButton1 = findViewById<Button>(R.id.food_switch)
+        switchButton1.setOnClickListener {
+            val intent = Intent(this, FoodRecommendation::class.java)
+            startActivity(intent)
+        }
+
+        val switchButton2 = findViewById<Button>(R.id.sleep_switch)
+        switchButton2.setOnClickListener {
+            val intent = Intent(this, Sleep::class.java)
             startActivity(intent)
         }
     }
