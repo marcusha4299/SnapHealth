8dbbd3c687c2cd44cd986f5f35a99db613125f8e
andynguyen4267 <83787736+andynguyen4267@users.noreply.github.com>
2024-03-05 15:14:45 -0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.code.gson:gson:2.8.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
