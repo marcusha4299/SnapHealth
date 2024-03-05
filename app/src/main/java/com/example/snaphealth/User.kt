5183eff4af0b1e5ef78d02bfd93f9a378c4ac301
andynguyen4267 <83787736+andynguyen4267@users.noreply.github.com>
2024-03-05 15:17:03 -0800

    Add files via upload

---
+++ app/src/main/java/com/example/snaphealth/User.kt
@@ -0,0 +1,12 @@
+package com.example.snaphealth
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val username: String,
+    val password: String,
+    val age: Int,
+    val height: Double,
+    val weight: Double,
+    val gender: Boolean
+)
